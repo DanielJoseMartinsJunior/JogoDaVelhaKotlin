dbc8092a3460ca7d14058ca8e94109775778d27e
DanielJoseMartinsJunior <daniel.jr2501@gmail.com>
2024-03-24 17:55:06 -0300

    Modificação na estrutura da tela

--- app/src/main/java/com/example/jogodavelhakotlin/MainActivity.kt
+++ app/src/main/java/com/example/jogodavelhakotlin/MainActivity.kt
@@ -4,16 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jogodavelhakotlin.ui.theme.JogoDaVelhaKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,11 +46,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JogoDaVelha(modifier: Modifier = Modifier) {
-    MainScreen()
+    TelaPrincipal()
 }
 
 @Composable
-fun MainScreen(){
+fun TelaPrincipal(modifier: Modifier = Modifier){
     Box(modifier = Modifier.fillMaxSize()) {
         Image(
             modifier = Modifier.fillMaxSize(),
@@ -47,12 +58,60 @@ fun MainScreen(){
             contentDescription = "background",
             contentScale = ContentScale.Crop
         )
+
+        Column (
+            modifier = modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ){
+            VezJogador(
+                jogador = "",
+                modifier = modifier
+            )
+
+            Tabuleiro(
+                modifier = modifier
+            )
+
+            JogarDeNovo(
+                modifier = modifier
+            )
+
+        }
     }
 }
 
+@Composable
+fun VezJogador(jogador: String, modifier: Modifier = Modifier){
+    Row (
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ){
+        Text(text = "Vez do Jogador: $jogador", color = Color.Black, fontSize = 26.sp)
+    }
+}
+
+@Composable
+fun Tabuleiro(modifier: Modifier = Modifier){
+
+}
+
+@Composable
+fun JogarDeNovo(modifier: Modifier = Modifier){
+    Column (horizontalAlignment = Alignment.CenterHorizontally){
+        Text(text = "")
+        Button(onClick = { }) {
+            Text(text = "Jogar de Novo")
+        }
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     JogoDaVelhaKotlinTheme {
         JogoDaVelha()
     }
