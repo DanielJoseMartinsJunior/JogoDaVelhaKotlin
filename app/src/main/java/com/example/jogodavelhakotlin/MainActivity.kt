72746d01d2f38b7431b0f53610ae6fba48ca4c61
Gabriel <gabrielhl25@hotmail.com>
2024-03-24 19:07:30 -0300

    Criação do tabuleiro

--- app/src/main/java/com/example/jogodavelhakotlin/MainActivity.kt
+++ app/src/main/java/com/example/jogodavelhakotlin/MainActivity.kt
@@ -8,19 +8,25 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,6 +57,9 @@ fun JogoDaVelha(modifier: Modifier = Modifier) {
 
 @Composable
 fun TelaPrincipal(modifier: Modifier = Modifier){
+    var jogadorAtual by remember { mutableStateOf("X") } // Estado para controlar o jogador atual
+    var tabuleiro1 by remember { mutableStateOf(Array(3) { Array(3) { "" } }) } // Estado para representar o tabuleiro
+
     Box(modifier = Modifier.fillMaxSize()) {
         Image(
             modifier = Modifier.fillMaxSize(),
@@ -60,17 +69,24 @@ fun TelaPrincipal(modifier: Modifier = Modifier){
         )
 
         Column (
-            modifier = modifier.fillMaxWidth(),
+            modifier = modifier.fillMaxWidth().fillMaxHeight(0.8f),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceBetween
         ){
             VezJogador(
-                jogador = "",
+                jogador = jogadorAtual,
                 modifier = modifier
             )
 
             Tabuleiro(
-                modifier = modifier
+                modifier = modifier,
+                onCelulaClicked = { row, col ->
+                    if (tabuleiro1[row][col].isEmpty()) {
+                        tabuleiro1[row][col] = jogadorAtual // Faz a jogada na matriz do tabuleiro
+                        jogadorAtual = if (jogadorAtual == "X") "O" else "X" // Alterna o jogador
+                    }
+                },
+                tabuleiro = tabuleiro1
             )
 
             JogarDeNovo(
@@ -92,11 +108,41 @@ fun VezJogador(jogador: String, modifier: Modifier = Modifier){
         Text(text = "Vez do Jogador: $jogador", color = Color.Black, fontSize = 26.sp)
     }
 }
+@Composable
+fun Tabuleiro(modifier: Modifier = Modifier,
+              onCelulaClicked: (row: Int, col: Int) -> Unit,
+              tabuleiro: Array<Array<String>>) {
+
+    Column(modifier = modifier) {
+        for (i in tabuleiro.indices) {
+            Row {
+                for (j in tabuleiro[i].indices) {
+                    Celula(
+                        text = tabuleiro[i][j],
+                        onClick = { onCelulaClicked(i, j) },
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+            }
+        }
+    }
+}
 
 @Composable
-fun Tabuleiro(modifier: Modifier = Modifier){
+fun Celula(text: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .padding(4.dp)
+            .aspectRatio(1f),
+        shape = RoundedCornerShape(0.dp),
 
+
+    ) {
+        Text(text = text, fontSize = 50.sp, color = Color.White)
+    }
 }
+
 
 @Composable
 fun JogarDeNovo(modifier: Modifier = Modifier){
